1756c0ef3bdde2adef0e474828c99318a80fe79a
Tom Camp <tom.camp@civicactions.com>
2026-01-29 10:25:12 -0500

    feat: adding importlib to load config dynamically.

--- allusgov/settings.py
+++ allusgov/settings.py
@@ -1,72 +1,14 @@
 # Settings for allusgov project
-#
-
-from .exporter import exporter
-from .importer import importer, samgov_importer, digitalregistry_importer
 from .processor import normalize_name
-from .spider import (
-    budget,
-    cisagov,
-    federalregister,
-    opmgov,
-    samgov,
-    usagov,
-    usaspending,
-    usgovmanual,
-    digitalregistry,
-)
+from .utils.yaml_loader import load_yaml_with_imports
+
+config: dict = load_yaml_with_imports("allusgov/config.yaml")
 
 # Source settings
-SOURCES = {
-    "samgov": {
-        "importer": samgov_importer.SamgovImporter,
-        "spider": samgov.SamgovSpider,
-    },
-    "budget": {
-        "importer": importer.Importer,
-        "spider": budget.BudgetSpider,
-    },
-    "cisagov": {
-        "importer": importer.Importer,
-        "spider": cisagov.CisagovSpider,
-    },
-    "opmgov": {
-        "importer": importer.Importer,
-        "spider": opmgov.OpmgovSpider,
-    },
-    "usagov": {
-        "importer": importer.Importer,
-        "spider": usagov.UsagovSpider,
-    },
-    "usaspending": {
-        "importer": importer.Importer,
-        "spider": usaspending.UsaspendingSpider,
-    },
-    "federalregister": {
-        "importer": importer.Importer,
-        "spider": federalregister.FederalRegisterSpider,
-    },
-    "usgovmanual": {
-        "importer": importer.Importer,
-        "spider": usgovmanual.USGovManualSpider,
-    },
-    "digitalregistry": {
-        "importer": digitalregistry_importer.DigitalRegistryImporter,
-        "spider": digitalregistry.DigitalRegistrySpider,
-    },
-}
+SOURCES: dict = config.get("sources", {})
 
 # Exporter settings
-EXPORTERS = {
-    "text": exporter.TextExporter,
-    "json": exporter.JSONExporter,
-    "csv": exporter.CSVExporter,
-    "widecsv": exporter.WideCSVExporter,
-    "dot": exporter.DotExporter,
-    "gexf": exporter.GEXFExporter,
-    "graphml": exporter.GraphMLExporter,
-    "cyjs": exporter.CytoscapeJSONExporter,
-}
+EXPORTERS: dict = config.get("exporters", {})
 
 # Merge settings
 MERGE_BASE = "samgov"
